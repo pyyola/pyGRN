4e4ead00a8ea30692f60a85605ee0ec0a43d035c
pyyola <154836621+pyyola@users.noreply.github.com>
2024-08-21 18:09:31 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-API_TOKEN = '6317075903:AAExJcIjZFoKvoa9z0tbOEd702GiRh_BwQk'
+API_TOKEN = 'гавно'
 
-admin_id = [1414786309]
-pusher_ids = [1414786309, 5351012455, 792584311, 6427500914, 792584311]
+admin_id = [жопа]
+pusher_ids = [член]
 
 DATABASE = "database.db"
-CHANNEL_ID = -1002023506406
+CHANNEL_ID = ------
 
 bot = Bot(token=API_TOKEN)
 storage = MemoryStorage()
